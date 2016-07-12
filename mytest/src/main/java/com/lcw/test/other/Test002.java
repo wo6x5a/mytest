18452295958cdf4e1916bf013c6488b1f031176f
chenwulou <wo6x5a@163.com>
2016-07-12 16:22:36 +0800

    update

--- mytest/src/main/java/com/lcw/test/other/Test002.java
+++ mytest/src/main/java/com/lcw/test/other/Test002.java
@@ -1,13 +1,26 @@
 package com.lcw.test.other;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test002 {
 	public static void main(String[] args) {
-		int i = (101^101);
-		System.out.println(i);
-		Date newDate = new Date();
-		System.out.println(newDate);
-//		1438242246411
+//		int i = (101 ^ 101);
+//		System.out.println(i);
+//		Date newDate = new Date();
+//		System.out.println(newDate);
+//		// 1438242246411
+		StringBuilder resp = new StringBuilder("");
+		List<String> list = new ArrayList<>();
+		resp.append(1);
+		resp.append(",");
+		resp.append(2);
+		resp.append(",");
+		resp.append(3);
+		resp.append(",");
+		resp.append(4);
+		resp.append(",");
+		resp.delete(resp.length() - 1, resp.length());
+		System.out.println(resp);
 	}
 }
