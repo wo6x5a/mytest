133d7d8822263db5e6963a9ed007e5ad71549a64
chenwulou <wo6x5a@163.com>
2016-07-28 17:13:59 +0800

    update

---
+++ mytest/src/main/java/com/lcw/arithmetic/sort/BubbleSort.java
@@ -0,0 +1,57 @@
+package com.lcw.arithmetic.sort;
+
+/**
+ * 
+ * @author chenwulou
+ *
+ */
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		BubbleSort.bubble1();
+		System.out.println();
+		System.out.println("-----------------------------------------------------------------------------------");
+		BubbleSort.bubble2();
+	}
+
+	public static void bubble1() {
+		int is[] = { 3, 2, 4, 5, 9, 7, 6, 1, 8 };
+		int isLenth = is.length;
+		int sortNumber;
+		for (int i = 0; i < isLenth - 1; i++) { // 最多做n-1趟排序
+			for (int j = 0; j < isLenth - i - 1; j++) { // 对当前无序区间is[0......length-i-1]进行排序(j的范围很关键，这个范围是在逐步缩小的)
+				sortNumber = is[j];
+				if (sortNumber < is[j + 1]) { // 把小的值交换到后面
+					is[j] = is[j + 1];
+					is[j + 1] = sortNumber;
+				}
+			}
+			System.out.print("第" + (i + 1) + "次排序结果：");
+			for (int a = 0; a < is.length; a++) {
+				System.out.print(is[a] + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void bubble2() {
+		int is[] = { 3, 2, 4, 5, 9, 7, 6, 1, 8 };
+		int isLenth = is.length;
+		int sortNumber;
+		for (int i = 0; i < isLenth - 1; i++) {
+			for (int j = i + 1; j < isLenth; j++) {
+				sortNumber = is[i];
+				if (sortNumber < is[j]) {
+					is[i] = is[j];
+					is[j] = sortNumber;
+				}
+			}
+			System.out.print("第" + (i + 1) + "次排序结果:");
+			for (int a = 0; a < isLenth; a++) {
+				System.out.print(is[a] + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ mytest/src/main/java/com/lcw/test/array/TestArray.java
@@ -0,0 +1,9 @@
+package com.lcw.test.array;
+
+public class TestArray {
+
+	public static void main(String[] args) {
+		int[] a = new int[10];
+		System.out.println(a[2]);
+	}
+}
