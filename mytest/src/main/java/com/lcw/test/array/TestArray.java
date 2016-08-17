fc6a6718db9a3ccd9771a7e713765eb82a691e2e
chenwulou <wo6x5a@gmail.com>
2016-08-17 15:47:50 +0800

    update

--- mytest/src/main/java/com/lcw/test/array/TestArray.java
+++ mytest/src/main/java/com/lcw/test/array/TestArray.java
@@ -1,9 +1,20 @@
 package com.lcw.test.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestArray {
 
 	public static void main(String[] args) {
 		int[] a = new int[10];
 		System.out.println(a[2]);
+		List<String> lis = new ArrayList<>();
+		lis.add("11");
+        lis.add("22");
+        lis.add("33");
+        lis.add("44");
+        String lisStr = lis.toString();
+        System.out.println(lisStr.substring(1, lisStr.length()-1));
+        System.out.println(lis.toString());
 	}
 }
