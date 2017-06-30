5f5e249d44ee66c9dc2b79484f39baec5526f5f2
chenwulou <wo6x5a@gmail.com>
2017-06-30 11:52:55 +0800

    add queue test
    Signed-off-by: chenwulou <wo6x5a@gmail.com>

--- mytest/src/main/java/com/lcw/test/other/Test014.java
+++ mytest/src/main/java/com/lcw/test/other/Test014.java
@@ -1,7 +1,23 @@
 package com.lcw.test.other;
 
-public class Test014 {
-	public static void main(String[] args) {
+import java.util.Calendar;
+import java.util.Date;
 
-	}
+public class Test014 {
+
+  public static final Date testCr(Date fullDate) {
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(fullDate);
+    cal.set(11, 0);
+    cal.set(12, 0);
+    cal.set(13, 0);
+    cal.set(14, 0);
+    return cal.getTime();
+  }
+
+  public static void main(String[] args) {
+    Date date = new Date();
+    System.out.println(date);
+    System.out.println(Test014.testCr(date));
+  }
 }
